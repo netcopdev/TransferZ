44ebf4d85f7bf0ba51a04307082c9ec38bfc7ed0
Hristov <mr.hristo.hristov@gmail.com>
2026-09-10 20:31:18 +0300

    Reset stale transient routing state

--- Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
+++ Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
@@ -1,8 +1,26 @@
 modded class MissionGameplay
 {
+    protected float m_TransferZTransientCheckAccumulator;
+
     override void OnInit()
     {
         super.OnInit();
         Print("[TransferZ] MissionGameplay.OnInit - 5_Mission module loaded");
+    }
+
+    override void OnUpdate(float timeslice)
+    {
+        super.OnUpdate(timeslice);
+
+        if (!GetGame().GetPlayer())
+            return;
+
+        m_TransferZTransientCheckAccumulator += timeslice;
+        if (m_TransferZTransientCheckAccumulator < 0.25)
+            return;
+
+        m_TransferZTransientCheckAccumulator = 0.0;
+        if (TransferZClientState.Get().ValidateTransientState())
+            TransferZHeaderControls.RefreshAll();
     }
 }
