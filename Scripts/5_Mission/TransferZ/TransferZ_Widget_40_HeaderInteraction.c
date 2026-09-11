02e198f9acd3b10939205d0569d9c9e881592ff7
Hristov <mr.hristo.hristov@gmail.com>
2026-09-11 14:04:03 +0300

    Preserve native header geometry during deferred layout

--- Scripts/5_Mission/TransferZ/TransferZ_Widget_40_HeaderInteraction.c
+++ Scripts/5_Mission/TransferZ/TransferZ_Widget_40_HeaderInteraction.c
@@ -8,7 +8,6 @@ modded class TransferZHeaderControls
         if (!m_Root || !m_HeaderLabel || !m_Entity || !m_Entity.GetInventory().GetCargo())
             return;
 
-        RestoreHeaderText();
         if (m_HeaderHost)
             m_HeaderHost.Update();
         m_HeaderLabel.Update();
