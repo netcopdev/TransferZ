0d989277363a70e479c9ae4d6a1bca4ca455cfdc
Hristov <mr.hristo.hristov@gmail.com>
2026-09-17 09:53:00 +0300

    refactor(sort): route RPC sort through external staging planner

--- Scripts/4_World/TransferZ/TransferZ_CFModule.c
+++ Scripts/4_World/TransferZ/TransferZ_CFModule.c
@@ -5,7 +5,7 @@ class TransferZCFModule : CF_ModuleWorld
     {
         super.OnInit();
         EnableRPC();
-        Print("[TransferZ] BUILD MARKER SORT-V6-20260917-A");
+        Print("[TransferZ] BUILD MARKER SORT-EXTERNAL-20260917-A");
     }
 
     override int GetRPCMin()
@@ -42,6 +42,6 @@ class TransferZCFModule : CF_ModuleWorld
         else if (rpc.ID == TransferZNestedUnpackRPC.REQUEST)
             TransferZNestedUnpackService.HandleRequest(player, rpc.Sender, rpc.Context);
         else if (rpc.ID == TransferZMaintenanceRPC.REQUEST)
-            TransferZSortPlannerV6.HandleRequest(player, rpc.Sender, rpc.Context);
+            TransferZExternalStagingSortPlanner.HandleRequest(player, rpc.Sender, rpc.Context);
     }
 }
