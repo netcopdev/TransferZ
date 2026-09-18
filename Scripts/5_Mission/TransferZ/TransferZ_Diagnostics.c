f8f1dda1cc7dd68d1df4f5953858cb861527d7b2
Hristov <mr.hristo.hristov@gmail.com>
2026-09-18 12:41:01 +0300

    debug(drag): bump resolver marker after compile-order fix

--- Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
+++ Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
@@ -6,7 +6,7 @@ modded class MissionGameplay
     {
         super.OnInit();
         Print("[TransferZ] MissionGameplay.OnInit - 5_Mission module loaded");
-        Print("[TransferZ] DRAG-RESOLVER-BUILD-20260918-B");
+        Print("[TransferZ] DRAG-RESOLVER-BUILD-20260918-C");
     }
 
     override void OnUpdate(float timeslice)
