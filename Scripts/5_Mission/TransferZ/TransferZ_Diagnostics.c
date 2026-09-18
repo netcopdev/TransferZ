3ff3343cc579677001746dff56c9401144e6976b
Hristov <mr.hristo.hristov@gmail.com>
2026-09-18 11:07:45 +0300

    debug(drag): bump resolver runtime marker

--- Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
+++ Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
@@ -6,7 +6,7 @@ modded class MissionGameplay
     {
         super.OnInit();
         Print("[TransferZ] MissionGameplay.OnInit - 5_Mission module loaded");
-        Print("[TransferZ] DRAG-RESOLVER-BUILD-20260918-A");
+        Print("[TransferZ] DRAG-RESOLVER-BUILD-20260918-B");
     }
 
     override void OnUpdate(float timeslice)
