8a148a602b7eb7fb10acdda4279a6de547845412
Hristov <mr.hristo.hristov@gmail.com>
2026-09-19 05:56:20 +0300

    chore(log): remove temporary runtime build markers

--- Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
+++ Scripts/5_Mission/TransferZ/TransferZ_Diagnostics.c
@@ -2,13 +2,6 @@ modded class MissionGameplay
 {
     protected float m_TransferZTransientCheckAccumulator;
 
-    override void OnInit()
-    {
-        super.OnInit();
-        Print("[TransferZ] MissionGameplay.OnInit - 5_Mission module loaded");
-        Print("[TransferZ] DRAG-RESOLVER-BUILD-20260918-D");
-    }
-
     override void OnUpdate(float timeslice)
     {
         super.OnUpdate(timeslice);
