aecbd8edf6d5de1910b4987c5b85cc11a7845ee9
Hristov <mr.hristo.hristov@gmail.com>
2026-09-19 18:35:25 +0300

    feat(sort): add non-interactive sort buffer entity

---
+++ Scripts/4_World/TransferZ/TransferZ_SortBuffer.c
@@ -0,0 +1,44 @@
+class TransferZ_SortBuffer : Container_Base
+{
+    override void EEInit()
+    {
+        super.EEInit();
+        SetAllowDamage(false);
+        DisableSimulation(true);
+    }
+
+    override bool IsInventoryVisible()
+    {
+        return false;
+    }
+
+    override bool CanDisplayCargo()
+    {
+        return false;
+    }
+
+    override bool IsTakeable()
+    {
+        return false;
+    }
+
+    override bool CanPutIntoHands(EntityAI parent)
+    {
+        return false;
+    }
+
+    override bool CanPutInCargo(EntityAI parent)
+    {
+        return false;
+    }
+
+    override bool IsActionTargetVisible()
+    {
+        return false;
+    }
+
+    override bool CanBeActionTarget()
+    {
+        return false;
+    }
+}
