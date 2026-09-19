499211cf6f944e25de95ab2d45d4bcaceb552898
Hristov <mr.hristo.hristov@gmail.com>
2026-09-19 05:56:52 +0300

    chore(log): remove routine sort client diagnostics

--- Scripts/5_Mission/TransferZ/TransferZ_MaintenanceClient.c
+++ Scripts/5_Mission/TransferZ/TransferZ_MaintenanceClient.c
@@ -31,44 +31,6 @@ class TransferZMaintenanceClient
         return duplicate;
     }
 
-    protected static void LogSortCargo(EntityAI source)
-    {
-        if (!source)
-            return;
-
-        CargoBase cargo = source.GetInventory().GetCargo();
-        if (!cargo)
-            return;
-
-        Print("[TransferZ] Sort cargo snapshot source=" + source.GetType() + " grid=" + cargo.GetWidth().ToString() + "x" + cargo.GetHeight().ToString() + " items=" + cargo.GetItemCount().ToString());
-        for (int cargoItemIndex = 0; cargoItemIndex < cargo.GetItemCount(); cargoItemIndex++)
-        {
-            EntityAI item = cargo.GetItem(cargoItemIndex);
-            if (!item)
-                continue;
-
-            InventoryLocation location = new InventoryLocation();
-            if (!item.GetInventory().GetCurrentInventoryLocation(location))
-            {
-                Print("[TransferZ] Sort cargo item index=" + cargoItemIndex.ToString() + " type=" + item.GetType() + " location=unavailable");
-                continue;
-            }
-
-            int itemWidth;
-            int itemHeight;
-            cargo.GetItemSize(cargoItemIndex, itemWidth, itemHeight);
-            bool flip = location.GetFlip();
-            if (flip)
-            {
-                int orientationSwap = itemWidth;
-                itemWidth = itemHeight;
-                itemHeight = orientationSwap;
-            }
-
-            Print("[TransferZ] Sort cargo item index=" + cargoItemIndex.ToString() + " type=" + item.GetType() + " row=" + location.GetRow().ToString() + " col=" + location.GetCol().ToString() + " size=" + itemWidth.ToString() + "x" + itemHeight.ToString() + " flip=" + flip.ToString());
-        }
-    }
-
     protected static bool Request(int operation, EntityAI source)
     {
         PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
@@ -76,28 +38,20 @@ class TransferZMaintenanceClient
         {
             if (operation == TransferZMaintenanceOperation.SORT)
             {
-                Print("[TransferZ] Sort client rejected: source is not reachable cargo");
                 TransferZMaintenanceResultState.PublishLocal(operation, source, false);
             }
             return false;
         }
         if (IsDuplicate(operation, source))
         {
-            if (operation == TransferZMaintenanceOperation.SORT)
-                Print("[TransferZ] Sort client rejected: duplicate request debounce");
             return false;
         }
-
-        if (operation == TransferZMaintenanceOperation.SORT)
-            LogSortCargo(source);
 
         if (!GetGame().IsMultiplayer())
         {
             if (operation == TransferZMaintenanceOperation.SORT)
             {
-                Print("[TransferZ] Sort client executing local DayZDiag/single-player request");
                 int sortResult = TransferZExternalStagingSortPlanner.Sort(player, source);
-                Print("[TransferZ] Sort client local result=" + sortResult.ToString());
                 TransferZMaintenanceResultState.PublishLocal(operation, source, sortResult >= 0);
                 return sortResult >= 0;
             }
@@ -113,9 +67,6 @@ class TransferZMaintenanceClient
         int sourceLow;
         int sourceHigh;
         TransferZNet.GetEntityNetworkId(source, sourceLow, sourceHigh);
-
-        if (operation == TransferZMaintenanceOperation.SORT)
-            Print("[TransferZ] Sort client sending RPC source=" + source.GetType() + " net=" + sourceLow.ToString() + ":" + sourceHigh.ToString());
 
         ScriptRPC rpc = new ScriptRPC();
         rpc.Write(operation);
