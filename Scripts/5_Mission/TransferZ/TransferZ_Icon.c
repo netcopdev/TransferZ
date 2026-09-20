225cf291bfade10fe9fb65316fa6dce9711a270e
Hristov <mr.hristo.hristov@gmail.com>
2026-09-20 16:34:06 +0300

    fix: preserve cargo grid identity

--- Scripts/5_Mission/TransferZ/TransferZ_Icon.c
+++ Scripts/5_Mission/TransferZ/TransferZ_Icon.c
@@ -7,6 +7,7 @@ modded class Icon
     protected int m_TransferZModifierDragMode = TRANSFERZ_MODIFIER_NONE;
     protected bool m_TransferZModifierDragStarted;
     protected EntityAI m_TransferZModifierDragSource;
+    protected int m_TransferZModifierDragSourceCargoIndex = 0;
 
     protected bool m_TransferZModifierClickRegistered;
     protected bool m_TransferZModifierClickPending;
@@ -23,8 +24,9 @@ modded class Icon
         }
     }
 
-    protected EntityAI TransferZGetDirectCargoSource()
+    protected EntityAI TransferZGetDirectCargoSource(out int cargoIndex)
     {
+        cargoIndex = 0;
         if (!m_Obj || m_HandsIcon)
             return null;
 
@@ -33,7 +35,8 @@ modded class Icon
             return null;
 
         EntityAI source = location.GetParent();
-        if (!source || !source.GetInventory().GetCargo())
+        cargoIndex = location.GetIdx();
+        if (!source || !TransferZCargo.Exists(source, cargoIndex))
             return null;
         return source;
     }
@@ -65,23 +68,24 @@ modded class Icon
         return true;
     }
 
-    protected bool TransferZRouteSingleDoubleClick(EntityAI source)
+    protected bool TransferZRouteSingleDoubleClick(EntityAI source, int sourceCargoIndex)
     {
         if (!source || !m_Obj)
             return false;
 
         TransferZClientState state = TransferZClientState.Get();
-        EntityAI linked = state.GetLinkedDestination(source);
+        EntityAI linked = state.GetLinkedDestination(source, sourceCargoIndex);
         if (linked)
-            return state.RequestMoveItem(m_Obj, linked);
+            return state.RequestMoveItem(m_Obj, linked, state.GetLinkedDestinationCargoIndex(source, sourceCargoIndex));
 
         if (TransferZIsPlayerInventorySource(source))
             return false;
 
         EntityAI destination = state.GetPreferredDestination();
-        if (!destination || destination == source || !state.CanPreferredAcceptItem(m_Obj, destination))
+        int destinationCargoIndex = state.GetPreferredCargoIndex();
+        if (!destination || (destination == source && destinationCargoIndex == sourceCargoIndex) || !state.CanPreferredAcceptItem(m_Obj, destination, destinationCargoIndex))
             return false;
-        return state.RequestMoveItem(m_Obj, destination);
+        return state.RequestMoveItem(m_Obj, destination, destinationCargoIndex);
     }
 
     protected int TransferZReadModifierDragMode()
@@ -103,6 +107,7 @@ modded class Icon
         m_TransferZModifierDragMode = TRANSFERZ_MODIFIER_NONE;
         m_TransferZModifierDragStarted = false;
         m_TransferZModifierDragSource = null;
+        m_TransferZModifierDragSourceCargoIndex = 0;
     }
 
     override void MouseClick(Widget w, int x, int y, int button)
@@ -110,7 +115,7 @@ modded class Icon
         if (button == MouseState.LEFT)
         {
             TransferZResetModifierDrag();
-            m_TransferZModifierDragSource = TransferZGetDirectCargoSource();
+            m_TransferZModifierDragSource = TransferZGetDirectCargoSource(m_TransferZModifierDragSourceCargoIndex);
             if (m_TransferZModifierDragSource && m_Obj)
                 m_TransferZModifierDragMode = TransferZReadModifierDragMode();
         }
@@ -134,13 +139,13 @@ modded class Icon
         InventoryLocation location = new InventoryLocation();
         if (!m_Obj.GetInventory().GetCurrentInventoryLocation(location))
             return;
-        if (location.GetType() != InventoryLocationType.CARGO || location.GetParent() != m_TransferZModifierDragSource)
+        if (!TransferZCargo.LocationMatches(location, m_TransferZModifierDragSource, m_TransferZModifierDragSourceCargoIndex))
             return;
 
         if (m_TransferZModifierDragMode == TRANSFERZ_MODIFIER_SHIFT)
-            TransferZOperationDrag.BeginContainer(TransferZOperation.TRANSFER, m_TransferZModifierDragSource);
+            TransferZOperationDrag.BeginContainer(TransferZOperation.TRANSFER, m_TransferZModifierDragSource, m_TransferZModifierDragSourceCargoIndex);
         else if (m_TransferZModifierDragMode == TRANSFERZ_MODIFIER_ALT)
-            TransferZOperationDrag.BeginClassTransfer(m_TransferZModifierDragSource, m_Obj);
+            TransferZOperationDrag.BeginClassTransfer(m_TransferZModifierDragSource, m_Obj, m_TransferZModifierDragSourceCargoIndex);
         else
             return;
 
@@ -186,8 +191,9 @@ modded class Icon
     {
         if (button == MouseState.LEFT && !g_Game.IsLeftCtrlDown() && !m_HandsIcon && m_Obj)
         {
-            EntityAI source = TransferZGetDirectCargoSource();
-            if (source && TransferZRouteSingleDoubleClick(source))
+            int sourceCargoIndex;
+            EntityAI source = TransferZGetDirectCargoSource(sourceCargoIndex);
+            if (source && TransferZRouteSingleDoubleClick(source, sourceCargoIndex))
                 return;
         }
 
